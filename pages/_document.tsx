c4363877e4d4b6ee1d9581e7b57ff857b42b6f1a
Felicio Mununga <felicio@users.noreply.github.com>
2023-02-17 23:02:28 +0100

    use tamagui css

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,80 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+import { Children } from 'react'
+import { AppRegistry } from 'react-native'
+
+import Tamagui from '../tamagui.config'
+
+// export default function Document() {
+//   return (
+//     <Html lang="en">
+//       <Head />
+//       <body>
+//         <Main />
+//         <NextScript />
+//       </body>
+//     </Html>
+//   )
+// }
+
+// TODO: the recommended approach breaks HMR when Tmaagui.getCSS() is called in the getInitialProps method.
+export default class Document extends NextDocument {
+  // static async getInitialProps({ renderPage }: any) {
+  //   AppRegistry.registerComponent('Main', () => Main)
+  //   const page = await renderPage()
+
+  //   // @ts-ignore
+  //   const { getStyleElement } = AppRegistry.getApplication('Main')
+
+  //   /**
+  //    * Note: be sure to keep tamagui styles after react-native-web styles like it is here!
+  //    * So Tamagui styles can override the react-native-web styles.
+  //    */
+  //   const styles = [
+  //     getStyleElement(),
+  //     <style
+  //       key="tamagui-css"
+  //       dangerouslySetInnerHTML={{ __html: Tamagui.getCSS() }}
+  //     />,
+  //   ]
+
+  //   return { ...page, styles: Children.toArray(styles) }
+  // }
+
+  static async getInitialProps({ renderPage }) {
+    AppRegistry.registerComponent('app', () => Main)
+    // @ts-ignore
+    const { getStyleElement } = AppRegistry.getApplication('app')
+
+    const page = await renderPage()
+
+    const styles = [
+      // <style dangerouslySetInnerHTML={{ __html: normalizeNextElements }} />,
+      getStyleElement(),
+      // <style
+      //   key="tamagui-css"
+      //   dangerouslySetInnerHTML={{ __html: Tamagui.getCSS() }}
+      // />,
+    ]
+
+    return { ...page, styles: Children.toArray(styles) }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <style
+            id="stitches"
+            dangerouslySetInnerHTML={{ __html: Tamagui.getCSS() }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
