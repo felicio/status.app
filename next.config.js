b4a33e454fe0f4607e4db3e95b33b2a1bfb85bda
Felicio Mununga <felicio@users.noreply.github.com>
2023-02-22 14:02:08 +0100

    update config

--- next.config.js
+++ next.config.js
@@ -1,55 +1,24 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
-
-// module.exports = nextConfig
-
-// /**
-//  * @type {import('next/dist/server/config-shared').NextConfig}
-//  */
-// export default {
-//   // reactStrictMode: true,
-//   reactStrictMode: false,
-//   experimental: {
-//     esmExternals: true,
-//   },
-// }
-
 /** @type {import('next').NextConfig} */
-// const { withTamagui } = require('@tamagui/next-plugin')
-// // const withImages = require('next-images')
-// const { join } = require('path')
-import pkg from '@tamagui/next-plugin'
+import nextTamagui from '@tamagui/next-plugin'
 import { join } from 'path'
 
-const { withTamagui } = pkg
+const { withTamagui } = nextTamagui
 
 process.env.IGNORE_TS_CONFIG_PATHS = 'true'
 process.env.TAMAGUI_TARGET = 'web'
 process.env.TAMAGUI_DISABLE_WARN_DYNAMIC_LOAD = '1'
 
-// const boolVals = {
-//   true: true,
-//   false: false,
-// }
-
-// const disableExtraction =
-//   boolVals[process.env.DISABLE_EXTRACTION] ??
-//   process.env.NODE_ENV === 'development'
-
 const plugins = [
-  // withImages,
   withTamagui({
     config: './tamagui.config.ts',
     components: [
+      // fixme?: works without it
+      // './node_modules/@status-im/components/packages/components/dist',
       // '@status-im/components'
-      './node_modules/@status-im/components/packages/components/dist',
     ],
     importsWhitelist: ['constants.js', 'colors.js'],
     logTimings: true,
     disableExtraction: true,
-
     // experiment - reduced bundle size react-native-web
     useReactNativeWebLite: false,
     shouldExtract: (path) => {
@@ -57,18 +26,10 @@ const plugins = [
         return true
       }
     },
-    excludeReactNativeWebExports: [
-      // 'Switch',
-      // 'ProgressBar',
-      // 'Picker',
-      // 'CheckBox',
-      // 'Touchable'
-    ],
   }),
 ]
 
-// module.exports = function () {
-export default function foo() {
+export default function config() {
   /** @type {import('next').NextConfig} */
   let config = {
     typescript: {
@@ -78,17 +39,12 @@ export default function foo() {
       disableStaticImages: true,
     },
     transpilePackages: [
-      //   'solito',
       'react-native-web',
-      //   'expo-linking',
-      //   'expo-constants',
-      //   'expo-modules-core'
-      '@status-im/components',
+      // fixme?: works without it
+      // '@status-im/components',
     ],
 
     experimental: {
-      // optimizeCss: true,
-      // scrollRestoration: true,
       legacyBrowsers: false,
     },
 
